610278056880d41bd05108cb16ff3db844045fa8
Daniel Szabo <daniel.szabo@crotive.com>
2024-01-25 13:16:43 +0800

    creating a sample table

---
+++ src/AccountTable/AccountTableContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AccountTable from './AccountTable'
+
+const AccountMgmt = () => {
+  return (
+    <div>
+        <div>Account Management</div>
+        <AccountTable/>
+    </div>
+  )
+}
+
+export default AccountMgmt
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import AccountTableContainer from './AccountTable/AccountTableContainer'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <div>
+      <AccountTableContainer/>
+    </div>
   )
 }
 
